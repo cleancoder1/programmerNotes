07062d871b9966034469877ad3b512b83405e24c
rparsi <rparsi@deltadentalia.com>
2018-08-27 13:16:48 -0500

    quick sort working changes , copied some code from textbook

---
+++ src/test/groovy/QuickSortSpec.groovy
@@ -0,0 +1,23 @@
+import spock.lang.Specification
+
+class QuickSortSpec extends Specification {
+
+    def " check if sorting worked"() {
+        given: ""
+        QuickSort quickSort = new QuickSort()
+
+        Comparable[] a = new Integer[6]
+        a[0] = 2
+        a[1] = 118
+        a[2] = 21
+        a[3] = 5
+        a[4] = 9
+        a[5] = 31
+        when: ""
+
+        quickSort.sort(a)
+        then: ""
+        quickSort.show(a)
+        true == quickSort.isSorted(a)
+    }
+}
