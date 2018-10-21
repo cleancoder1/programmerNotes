eddec22ebea0604a5652de41a8b4df65d1add73e
cleancoder1 <rajeshwar.parsi@gmail.com>
2018-10-21 08:19:26 -0500

    sorting alorithms

---
+++ src/main/java/FlipsMax.java
@@ -0,0 +1,37 @@
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+
+public class FlipsMax {
+
+
+    public static void main(String[] args) {
+        int t = Integer.parseInt(args[0]);
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+
+        for ( int i=0 ; i< t;i++){
+            if(StdRandom.bernoulli(0.5)){
+                heads.increment();
+            }else{
+                tails.increment();
+            }
+        }
+        if(heads.tally() == tails.tally()){
+            StdOut.println("Tie");
+        }else{
+            StdOut.println(max(heads,tails)+"wins");
+        }
+
+
+    }
+
+    public static Counter max(Counter x, Counter y){
+
+        if(x.tally() > y.tally()){
+            return x;
+        }
+        return y;
+    }
+}
