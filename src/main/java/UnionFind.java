f26b67ccb7798c63db6cd990fd5688b6c2512d5d
cleancoder1 <rajeshwar.parsi@gmail.com>
2018-11-04 22:35:31 -0600

    added union find algorithm , implementation

---
+++ src/main/java/UnionFind.java
@@ -0,0 +1,72 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class UnionFind {
+    private int[] id;
+    int size;
+
+
+    UnionFind(int n) {
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+        size = n;
+
+    }
+
+    void union(int p, int q) {
+        int rootOfp = root(p);
+        int rootOfq = root(q);
+
+        id[rootOfp] = rootOfq;
+        size--;
+    }
+
+    int count(){
+        return size;
+    }
+
+
+    int find(int p) {
+        return root(p);
+    }
+
+
+    int root(int p) {
+        //chase the pointer
+
+        while (id[p] != p) {
+
+            p = id[p];
+        }
+        return p;
+        //throw new IllegalArgumentException("never happens");
+
+    }
+
+
+    boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+
+    public static void main(String[] args) {
+
+        int N = StdIn.readInt();
+        UnionFind uf = new UnionFind(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) {
+                continue;
+            }
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+
+}
