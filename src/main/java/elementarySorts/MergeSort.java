f6146f1e690660a5a2e504497f8884628331f8c0
rparsi <rparsi@deltadentalia.com>
2018-11-15 11:18:31 -0600

    reimplemeneted merge sort , on own. Need to get better still.

--- src/main/java/elementarySorts/MergeSort.java
+++ src/main/java/elementarySorts/MergeSort.java
@@ -10,6 +10,76 @@ public class MergeSort extends Sort {
     @Override
     public void sort(Comparable[] a) {
 
-
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
     }
+
+    private void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    /*Given two sorted array from lo -mid inclusive , mid+1,hi  inclusive  how to do merge*/
+    /* copy the array into aux from lo - hi ,  */
+    private void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+
+        for (int i = lo; i <= hi; i++) {
+            aux[i] = a[i];
+        }
+
+        /* maintain two pointer one for first sorted array , other for second sorted array */
+
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+
+            /* left array is exhausted , copy all the right array elements */
+            if (i > mid) {
+                a[k] = aux[j];
+                j++;
+                continue;
+            }
+
+            /* right array is exhausted, copy all the left array elements */
+            if (j > hi) {
+                a[k] = aux[i];
+                i++;
+                continue;
+            }
+            if (less(aux[i], aux[j])) {
+                a[k] = aux[i];
+                i++;
+                continue;
+            }
+
+            if (!less(aux[i], aux[j])) {
+                a[k] = aux[j];
+                j++;
+                continue;
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        MergeSort mergeSort = new MergeSort();
+        Integer[] a = new Integer[5];
+        a[0] = 5;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 1;
+
+        mergeSort.sort(a);
+        mergeSort.show(a);
+    }
+
+
 }
